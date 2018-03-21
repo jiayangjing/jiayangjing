0e3291befeb1e6075c9d8d824d132ff267ddd414
jiayangjing <13753742265@163.com>
2018-03-21 20:47:43 +0800

    增加方法

--- springboot/src/main/java/com/dj/springboot01/controller/JyjController.java
+++ springboot/src/main/java/com/dj/springboot01/controller/JyjController.java
@@ -1,5 +1,15 @@
 package com.dj.springboot01.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
 public class JyjController {
 
+    @RequestMapping("")
+    @ResponseBody
+    public String get(){
+        return "";
+    }
 }
